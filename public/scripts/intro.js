4ef279105c60c90d48f772e86be8b28ae3b1252f
Andre Willian <andrehwillian@hotmail.com>
2020-06-08 15:44:21 -0300

    sending final project 08/06

---
+++ public/scripts/intro.js
@@ -0,0 +1,71 @@
+// document.write("Hello")
+
+// // let myvar = "He"
+// // const myconst = "HE"
+
+// // document.write(myvar + myconst)
+
+// // function sum(n1,n2){
+ 
+// //   document.write(n1+n2) 
+// // }
+  
+// // sum(1,2)
+
+
+
+
+
+// imc = (altura, peso) => {
+//   document.write (peso/(altura**2))
+// }
+
+// const André = {
+//   altura: 1.70,
+//   peso: 71.5
+// }
+
+// const Bruna = {
+//   altura: 1.55,
+//   peso: 58.5
+// }
+
+// function textobase() {
+//   document.write("Seu IMC é: ")
+// }
+
+// textobase()
+// imc(André.altura,André.peso)
+
+// textobase()
+// imc(Bruna.altura,Bruna.peso)
+  
+  
+
+
+
+
+
+// // const NotaFinal = 7
+
+// // if(NotaFinal < 5) {
+// //   document.write("Reprovado")
+// //   } else {
+// //     document.write("Aprovado")
+// //   }
+
+
+
+// // sum = () => "Ola";
+// // sum()
+
+// // function Hello() {
+// //   document.write("Hello World!");
+// // }
+
+
+
+
+// // Hello = () => "Hello World!";
+
+// // var Hello;
